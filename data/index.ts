8c1895c0bc4bc9309f8cd3a1ca770a905a29be38
1des 0f 1deas <idesofideas@protonmail.com>
2024-06-23 19:53:19 -0700

    updates

---
+++ data/index.ts
@@ -0,0 +1,222 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.png",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm flexible across distributed Earthly workflows.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "Quick to innovate. Slow to iterate.",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for solutions as a service.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building DEPIN software & neighbordhood business integrations",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+        id: 1,
+        title: "OathMakr",
+        des: "Provide bespoke music of any genre, your lyrics. Studioless, Bandless. Never Voiceless again.",
+        img: "/p1.svg",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/udio.png", "/trello.png"],
+        link: "https://www.oathmakr.com",
+    },
+    {
+        id: 2,
+        title: "Heritage Ventures",
+        des: "Fractional Ownership Platform for RWA's of museum quality.",
+        img: "/p2.svg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/thirdweb.png", "/ipfs.png"],
+        link: "https://hv-pxd3.vercel.app/",
+    },
+    {
+        id: 3,
+        title: "Dante's Inferno",
+        des: "From Poem to Literature to App to LLM. A modern retro homage to a literary giant",
+        img: "/p3.png",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/aceturnity.png", "/ipfs.png"],
+        link: "https://www.infernal-nfts.xyz",
+    },
+    {
+        id: 4,
+        title: "Paylyte",
+        des: "Invoices, upgraded for modern web speed.",
+        img: "/p4.png",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/c.svg", "/xmtp.png"],
+        link: "https://paylyte.vercel.app/",
+    },
+];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Full Stack Developer",
+      desc: "20 year of front-end UX/UI design for industry-standard form factors. 18 years server-side experience with DB's, python, Rust, CDNs",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.png",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native/Flutter.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.png",
+    },
+    {
+      id: 3,
+      title: "Product Manager, Architect, Fractional CTO",
+      desc: "Combined engineering/Sr. Developer XP, leading/co-leading several teams with product, roadmaps, strategic features rollouts",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.png",
+    },
+    {
+      id: 4,
+      title: "Blockchain, RWA, and DEPIN",
+      desc: "8 years in edge-technologies that are no longer the future. ",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.png",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/HollyNode", // Add link if needed
+        target: "_blank",
+    },
+    {
+        id: 2,
+        img: "/twit.svg",
+        link: "https://twitter.com/@infiniteteams", // Add link if needed
+        target: "_blank",
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "https://www.linkedin.com/in/matt-bauwens-a0572b1b/",
+        target: "_blank",
+    },
+];
